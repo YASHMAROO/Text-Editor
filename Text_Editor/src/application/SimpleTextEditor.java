5db1d04876a16d730d6943c9c58c04104c6728bb
YASHMAROO <marooyash@yahoo.in>
2020-09-28 11:32:37 +0530

    Added comments to the main code

--- Text_Editor/src/application/SimpleTextEditor.java
+++ Text_Editor/src/application/SimpleTextEditor.java
@@ -77,6 +77,7 @@ public class SimpleTextEditor{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// Creating a frame for the container
 		frame = new JFrame("seperator");
 		frame.setAutoRequestFocus(false);
 		frame.setTitle(title);
@@ -88,17 +89,20 @@ public class SimpleTextEditor{
 		textArea.setBackground(Color.WHITE);
 		textArea.setForeground(Color.BLACK);
 		textArea.setFont(new Font("Arial", Font.PLAIN, 14));
-		frame.getContentPane().add(textArea, BorderLayout.NORTH);
+		frame.getContentPane().add(textArea, BorderLayout.NORTH);//Adding text pane to the frame
 		
+		//Creating Scroll pane and adding it to the frame
 		JScrollPane scroll = new JScrollPane(textArea);
 		frame.getContentPane().add(scroll, BorderLayout.CENTER);
 		
+		// Creating menu bar and adding it to the frame
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setMargin(new Insets(0, 5, 0, 0));
 		menuBar.setBackground(Color.BLACK);
 		menuBar.setForeground(Color.WHITE);
 		frame.getContentPane().add(menuBar, BorderLayout.NORTH);
 		
+		//Adding panel to the frame
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.BLACK);
 		panel.setSize(100, 100);
@@ -107,7 +111,7 @@ public class SimpleTextEditor{
 		bold = new JButton();
 		bold.setText("Bold");
 		bold.setBackground(new java.awt.Color(0, 140, 255));
-		bold.addActionListener(e -> changeStyle());
+		bold.addActionListener(e -> changeStyle()); //Attaching action listener to listen for an event happening on a specified event
 		
 		italics = new JButton("Italics");
 		italics.setBackground(new java.awt.Color(0, 140, 255));
@@ -145,12 +149,14 @@ public class SimpleTextEditor{
 			}
 		});
 		
+		//Adding bold, italics, undo and redo buttons to the panel
 		panel.add(undo);
 		panel.add(redo);
 		panel.add(bold);
 		panel.add(italics);
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		
+		//Adding menu and menu items to the menu bar
 		JMenu mnNewMenu = new JMenu("File");
 		menuBar.add(mnNewMenu);
 		menuBar.add(new JMenu("|")).setEnabled(false);
@@ -220,9 +226,10 @@ public class SimpleTextEditor{
 			}
 		});
 		edit.add(cutOption);
-		edit.addSeparator();
+		edit.addSeparator();//Adding seperator in between the menu items to provide clartity
 		
 		JMenuItem copyOption = new JMenuItem("Copy");
+		//Attaching action listener to the menu items
 		copyOption.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
 				copyText();
@@ -249,13 +256,16 @@ public class SimpleTextEditor{
 		
 	}
 	
+	//We use try and catch block in file operations to handle any file I/O errors
+	//Function for opening a new file
 	private void open() {
 		try {
-			JFileChooser chooser = new JFileChooser();
-			chooser.setDialogTitle("Select a file To Open");
+			JFileChooser chooser = new JFileChooser();// Creating an object to choose file from your local machine
+			chooser.setDialogTitle("Select a file To Open"); // Setting the title of the choose file dialog box
 			chooser.showOpenDialog(null);
 			
-			File selectedFile = chooser.getSelectedFile();
+			File selectedFile = chooser.getSelectedFile();//Getting the file which the user has selected 
+			//Checking if user enters a file which doesn't exist and display an error message
 			if(!selectedFile.exists()) {
 				//Error message 
 				JOptionPane.showMessageDialog(null, "Failed to open a file, file doesn't exist", "Error", JOptionPane.ERROR_MESSAGE);
@@ -263,8 +273,9 @@ public class SimpleTextEditor{
 				return ;
 			}
 			
-			Scanner reader = new Scanner(selectedFile);
+			Scanner reader = new Scanner(selectedFile);// Reading the contents of a file through Scanner object
 			String contents = "";
+			//Reading the contents of the file until the end of file is not reached. 
 			while(reader.hasNextLine()) {
 				contents += reader.nextLine()+"\n";
 			}
@@ -280,12 +291,14 @@ public class SimpleTextEditor{
 		}
 	}
 	
+	//Function for saving a file with a new name (Save As)
 	private void create() {
 		try {
 			JFileChooser chooser = new JFileChooser();
 			chooser.setDialogTitle("Choose location to Save");
 			int save = chooser.showSaveDialog(null);
 			
+			// Checking if user doesnot want to save the file and exiting from the program 
 			if(save == JFileChooser.CANCEL_OPTION) {
 				System.exit(-1);
 			}
@@ -299,26 +312,29 @@ public class SimpleTextEditor{
 		}	
 	}
 	
+	//Function for saving a file
 	private void save() {
 		try {
-			String text = textArea.getText().trim();
+			String text = textArea.getText().trim();//Getting the text contents in the text pane
+			//Checking if text area is not empty and the opened File is null(A new document has been created)
 			if(!text.equals("") && openFile == null) {
 				create();
 				return ;
 			}
+			//Checking if opened file is null and the text area is also empty( The file cannot be saved)
 			if(openFile == null && text.equals("")) {
 				//Error Message
 				JOptionPane.showMessageDialog(null, "Can't Save , No file is selected", "Error", JOptionPane.ERROR_MESSAGE);
 				return ;
 			}
 			
-			String contents = textArea.getText();
+			String contents = textArea.getText();//Getting the contents of the text are
 			
-			Formatter form = new Formatter(openFile);
+			Formatter form = new Formatter(openFile);//Used for writing to new contents to the file
 			form.format("%s", contents);
-			form.close();
+			form.close();//Closing the form after using the formatter object
 			
-			frame.setTitle(title + " - " + openFile.getName());
+			frame.setTitle(title + " - " + openFile.getName());//Setting the title of the frame to the name of the opened file
 			
 			
 		} catch(Exception e) {
@@ -326,6 +342,7 @@ public class SimpleTextEditor{
 		}
 	}
 	
+	//Function to print the file
 	private void printFile() {
 		try { 
             // print the file 
@@ -336,28 +353,31 @@ public class SimpleTextEditor{
         }
 	}
 	
+	//Function for closing the file.
 	private void close() {
+		//Checking whether any file is opened or not
 		if(openFile == null) {
 			JOptionPane.showMessageDialog(null, "Failed to close the file,No file is selected", "Error", JOptionPane.ERROR_MESSAGE);
 			return ;
 		}
 		try {
-			
+			//Asking the user if he wants to save the file before closing it.
 			int input = JOptionPane.showConfirmDialog(null,"Do You want to save before closing?", "Wait!" ,JOptionPane.YES_NO_OPTION);
 			if(input == JOptionPane.YES_OPTION) {
 				save();
 			}
 			
-			textArea.setText("");
+			textArea.setText("");//Clearing the text pane
 			openFile = null;
-			frame.setTitle(title);
+			frame.setTitle(title);//Resetting the title of the frame
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+	//Function for creating a new text pane for a new file.
 	private void newFile() {
+		//Checking if any new file is opened or not
 		if(openFile != null) {
 			JOptionPane.showMessageDialog(null, "Please close or save the opened file", "Error", JOptionPane.ERROR_MESSAGE);
 			return ;
@@ -365,8 +385,9 @@ public class SimpleTextEditor{
 		textArea.setText("");
 	}
 	
+	//Functions for cut,copy and paste operations
 	private void cutText() {
-		textArea.cut();
+		textArea.cut();//Predefined method in JTexField class to perform cut function
 	}
 	
 	private void copyText() {
@@ -378,23 +399,26 @@ public class SimpleTextEditor{
 	}
 	
 	private void exitTheFrame() {
-		System.exit(0);
+		System.exit(0);//Closing the whole frame at once(Terminating the program)
 	}
 	
+	//Functions for changing style of the text (Bold and Italics)
 	private void changeStyle() {
 		StyledDocument doc = (StyledDocument) textArea.getDocument();
+		//Getting the start and end position of the selected text in the text field
 	    int selectionEnd = textArea.getSelectionEnd();
 	    int selectionStart = textArea.getSelectionStart();
+	    //If no text is selected set the control outside the function
 	    if (selectionStart == selectionEnd) {
 	      return;
 	    }
-	    Element element = doc.getCharacterElement(selectionStart);
-	    AttributeSet as = element.getAttributes();
+	    Element element = doc.getCharacterElement(selectionStart);// Gets the element that represents the character that is at the given offset within the document
+	    AttributeSet as = element.getAttributes(); //Fetches the value of the given attribute
 
 	    MutableAttributeSet asNew = new SimpleAttributeSet(as.copyAttributes());
 	    StyleConstants.setBold(asNew, !StyleConstants.isBold(as));
 	    doc.setCharacterAttributes(selectionStart, textArea.getSelectedText()
-	        .length(), asNew, true);
+	        .length(), asNew, true);//Changes the content element attributes used for the given range of existing content in the document. 
 	    String text = (StyleConstants.isBold(as) ? "Cancel Bold" : "Bold");
 	    bold.setText(text);
 	  }
